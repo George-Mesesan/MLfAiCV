1b0784abfc052130b015ba94adaa4addc7b96b39
George-Mesesan <gme_public@yahoo.com>
2015-04-23 22:03:18 +0200

    final version

--- MLfAiCV1/src/train/svm.py
+++ MLfAiCV1/src/train/svm.py
@@ -1,10 +1,7 @@
 from train.util import readData, train
 from sklearn import svm
 
-trainImages, trainLabels = readData('train', 60000)
+trainImages, trainLabels = readData('train', 2000)
 testImages, testLabels = readData('t10k', 10000)
 
-train(svm.SVC(kernel='rbf'), trainImages, trainLabels, testImages, testLabels)
 train(svm.SVC(kernel='linear'), trainImages, trainLabels, testImages, testLabels)
-train(svm.SVC(kernel='poly'), trainImages, trainLabels, testImages, testLabels)
-train(svm.LinearSVC(), trainImages, trainLabels, testImages, testLabels)
